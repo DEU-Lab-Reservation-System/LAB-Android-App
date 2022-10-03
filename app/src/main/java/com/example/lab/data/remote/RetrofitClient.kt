56c309b3f916433d15ba6a4e4d8a4a9b295e7727
donghyeon0915 <dongdong0915_@naver.com>
2022-10-04 03:29:07 +0900

    ADD : ApiLogger 추가 + JSON PrettyPrint 설정

--- app/src/main/java/com/example/lab/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/lab/data/remote/RetrofitClient.kt
@@ -1,12 +1,16 @@
 package com.example.lab.data.remote
 
+import com.example.lab.utils.ApiLogger
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
     private const val BASE_URL = "http://43.200.142.116"
+
     var retrofit: Retrofit
 
     init {
@@ -14,8 +18,16 @@ object RetrofitClient {
             .setDateFormat("yyyy-MM-dd")
             .create()
 
+        val interceptor = HttpLoggingInterceptor(ApiLogger())
+
+        // Http 요청/응답 중 Body만 로깅
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client:OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
